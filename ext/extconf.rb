fa3ea38d44bdfef80234f4bda7781deaab5ca5a7
Andrew Mains <andrewmains12@berkeley.edu>
2012-11-21 02:53:52 -0800

    Added glpk binaries

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,12 @@
 require 'mkmf'
+
+SCRIPT_DIR = File.dirname(File.absolute_path(__FILE__))
+GLPK_DIR = "#{SCRIPT_DIR}/glpk"
 dir_config('glpk')
+
+find_library("glpk", nil, "#{GLPK_DIR}/lib")
+find_header("glpk.h", "#{GLPK_DIR}/include")
+
 unless have_library("glpk")
   $stderr.puts "\nERROR: Cannot find the GLPK library, aborting."
   exit 1
