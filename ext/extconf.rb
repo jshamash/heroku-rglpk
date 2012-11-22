9b7944cd948997f4e177965748e88399f515edad
Andrew Mains <andrewmains12@berkeley.edu>
2012-11-22 01:47:17 -0800

    Made extconf compile glpk when it can

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,10 +4,13 @@ require 'pathname'
 ROOT_DIR = File.dirname(File.absolute_path(__FILE__))
 PROG_NAME = "glpk-4.44"
 
-TARBALL_NAME = "#{PROG_NAME}.tar.gz"
+TARBALL_PATH = "#{ROOT_DIR}/#{PROG_NAME}.tar.gz"
 GLPK_DIR = "#{ROOT_DIR}/glpk-4.44"
 GLPK_INSTALL = "#{ROOT_DIR}/glpk_install"
 def install_glpk
+#  system "curl -o #{TARBALL_PATH} http://ftp.gnu.org/gnu/glpk/glpk-4.44.tar.gz"
+  Dir.chdir ROOT_DIR
+  system "tar -xzf #{TARBALL_PATH}"
   res = system "cd #{GLPK_DIR} && ./configure --prefix=#{GLPK_INSTALL} && make && make install"
   if not res
     puts "Failed to build GLPK, aborting"
@@ -15,7 +18,12 @@ def install_glpk
   end
 end
 
-#install_glpk
+unless RUBY_PLATFORM =~ /x86_64-linux/
+  install_glpk
+end
+
+
+
 dir_config('glpk')
 find_library("glpk", nil, "#{GLPK_INSTALL}/lib")
 find_header("glpk.h", "#{GLPK_INSTALL}/include")
