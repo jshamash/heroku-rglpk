ff58085b0de48fb3eb6b785742f659fbbd8a7990
Andrew Mains <andrewmains12@berkeley.edu>
2012-11-21 19:03:35 -0800

    Added binaries for glpk

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,29 +5,29 @@ ROOT_DIR = File.dirname(File.absolute_path(__FILE__))
 PROG_NAME = "glpk-4.44"
 
 TARBALL_NAME = "#{PROG_NAME}.tar.gz"
-GLPK_DIR = "#{ROOT_DIR}/#{PROG_NAME}"
-GLPK_INSTALL = "#{ROOT_DIR}/glpk_install"
-def install_glpk
-  # puts "Downloading glpk"
-  # system "curl http://ftp.gnu.org/gnu/glpk/glpk-4.44.tar.gz -O" 
+GLPK_DIR = "#{ROOT_DIR}/glpk"
+# GLPK_INSTALL = "#{ROOT_DIR}/glpk_install"
+# def install_glpk
+#   # puts "Downloading glpk"
+#   # system "curl http://ftp.gnu.org/gnu/glpk/glpk-4.44.tar.gz -O" 
 
-  puts "Unpacking glpk"
-  system "tar -xzf #{ROOT_DIR}/#{TARBALL_NAME}"
-  puts 
-  puts "Configuring..."
+#   puts "Unpacking glpk"
+#   system "tar -xzf #{ROOT_DIR}/#{TARBALL_NAME}"
+#   puts 
+#   puts "Configuring..."
 
-  res = system "cd #{GLPK_DIR} && ./configure --prefix=#{GLPK_INSTALL} && make && make install"
-  if not res
-    puts "Failed to build GLPK, aborting"
-    exit 1
-  end
-end
+#   res = system "cd #{GLPK_DIR} && ./configure --prefix=#{GLPK_INSTALL} && make && make install"
+#   if not res
+#     puts "Failed to build GLPK, aborting"
+#     exit 1
+#   end
+# end
 
-install_glpk
+# install_glpk
 dir_config('glpk')
 
-find_library("glpk", nil, "#{GLPK_INSTALL}/lib")
-find_header("glpk.h", "#{GLPK_INSTALL}/include")
+find_library("glpk", nil, "#{GLPK_DIR}/lib")
+find_header("glpk.h", "#{GLPK_DIR}/include")
 
 unless have_library("glpk")
   $stderr.puts "\nERROR: Cannot find the GLPK library, aborting."
